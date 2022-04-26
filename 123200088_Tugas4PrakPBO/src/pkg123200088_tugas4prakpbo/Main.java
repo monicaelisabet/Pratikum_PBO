504b677619ef70e104be5c595bd5cb2ce0abe1fc
Yuni Safitri <yunisafitrix@gmail.com>
2022-04-26 23:58:13 +0700

    Tugas 4 Prak Pbo

---
+++ 123200088_Tugas4PrakPBO/src/pkg123200088_tugas4prakpbo/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg123200088_tugas4prakpbo;
+
+/**
+ *
+ * @author yunis
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Connector connect = new Connector();
+        GUI GUI = new GUI();
+        // TODO code application logic here
+    }
+    
+}
